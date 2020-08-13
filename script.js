3c0ee77c21a2ac53ad954d7bbbdcf2339064f158
jero-mil <jeromilrodil1@gmail.com>
2020-08-13 17:28:00 +0800

    fdfjdmgdh

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
-  if (document.body.scrollTop >= 20) {
-    document.getElementById("botom").style.display = "block";
-    document.getElementById("side").style.display = "none";
-  } else {
-    document.getElementById("botom").style.display = "none";
-    document.getElementById("side").style.display = "block";
+  if (document.getElementById("btn-arrow").style.maxWidth = "720px") {
+    document.getElementById("btn-arrow").style.display = "none";
+    if (document.body.scrollTop >= 20) {
+      document.getElementById("btn-arrow").style.display = "block";
+      document.getElementById("botom").style.display = "block";
+      document.getElementById("side").style.display = "none";
+    }
+    else {
+      document.getElementById("botom").style.display = "none";
+      document.getElementById("side").style.display = "block";
+    }
   }
+
 }
+
 
 function togle() {
   var element = document.getElementById("side");
@@ -16,10 +23,10 @@ function togle() {
 
   if (element.style.display == "none") {
     element.style.display = "block";
-    element2.style.display="none";
+    element2.style.display = "none";
   }
-else {
-  element.style.display = "none";
-  element2.style.display="block";
+  else {
+    element.style.display = "none";
+    element2.style.display = "block";
   }
 }
