0887ee4a88531e478f9b7dcda7a9457b397be1f2
jero-mil <jeromilrodil1@gmail.com>
2020-08-13 11:55:58 +0800

    regarddfb

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
-  if (document.body.scrollTop >= 20 ) {
+  if (document.body.scrollTop >= 20) {
     document.getElementById("botom").style.display = "block";
     document.getElementById("side").style.display = "none";
   } else {
     document.getElementById("botom").style.display = "none";
     document.getElementById("side").style.display = "block";
+  }
+}
+
+function togle() {
+  var element = document.getElementById("side");
+  var element2 = document.getElementById("botom");
+
+  if (element.style.display == "none") {
+    element.style.display = "block";
+    element2.style.display="none";
+  }
+else {
+  element.style.display = "none";
+  element2.style.display="block";
   }
 }
