d269f139d2d9b522c72a27465f206b8f571ba53d
jero-mil <jeromilrodil1@gmail.com>
2020-08-15 13:22:47 +0800

    Semi-final

--- script.js
+++ script.js
@@ -1,29 +1,11 @@
 window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
-    if (document.body.scrollTop >= 20) {
-      document.getElementById("botom").style.display = "block";
-      document.getElementById("side").style.display = "none";
-    }
-    else {
-      document.getElementById("botom").style.display = "none";
-      document.getElementById("side").style.display = "block";
-    }
-  }
-
-
-
-
-function togle() {
-  var element = document.getElementById("side");
-  var element2 = document.getElementById("botom");
-
-  if (element.style.display == "none") {
-    element.style.display = "block";
-    element2.style.display = "none";
+  var btn = document.getElementById("botom");
+  if (document.body.scrollTop >= 20) {
+    btn.style.display = "block";
   }
   else {
-    element.style.display = "none";
-    element2.style.display = "block";
+    btn.style.display = "none";
   }
 }
